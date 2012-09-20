2469ce3af65b2b87120e35eb86b2463e9fa02213
Matt Jordan <mjordan@mines.edu>
2012-09-20 09:14:07 -0600

    Shazam!

--- src/MagicFrame.java
+++ src/MagicFrame.java
@@ -1,8 +1,17 @@
+import javax.swing.JFrame;
 
-public class MagicFrame {
+
+public class MagicFrame extends JFrame {
 
 	public MagicFrame() {
-		// TODO Auto-generated constructor stub
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 600);
+		setResizable(false);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		JFrame frame = new MagicFrame();
 	}
 
 }
